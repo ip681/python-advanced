c66c71f4949a68a00f071a7910d908727888b8f0
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-10-07 16:57:40 +0300

    Create 1_reverse_strings.py

---
+++ 1. Lists as Stacks and Queues/Lists as Stacks and Queues - Lab/1_reverse_strings.py
@@ -0,0 +1,14 @@
+stack_list = list(input())
+while stack_list:
+    print(stack_list.pop(), end= "")
+
+
+# from collections import deque
+# stack = deque(input())
+# stack.reverse()
+# print(*stack, sep="")
+
+
+
+
+# print(input()[::-1])
